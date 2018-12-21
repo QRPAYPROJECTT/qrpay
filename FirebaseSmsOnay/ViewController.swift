70474a6c7bb29c181cada9d216642227f8021b41
imac2 <imac2@imac2s-imac.local>
2018-12-21 11:36:52 +0300

    Initial Commit

---
+++ FirebaseSmsOnay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseSmsOnay
+//
+//  Created by imac2 on 12/21/18.
+//  Copyright © 2018 imac2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
